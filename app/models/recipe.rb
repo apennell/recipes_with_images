72b7b8b43dbe57092cafa22abb33d988adb71742
Annie Pennell <annie@anniepennell.com>
2015-05-05 15:30:47 -0700

    initial commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :avatar, :less_than => 10.megabytes
 end
